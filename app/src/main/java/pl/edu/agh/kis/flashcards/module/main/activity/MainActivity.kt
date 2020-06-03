d175812b91c249ea441ca2df2b10ebce4c10be2a
Pawel Galka <p.k.galka@gmail.com>
2020-06-03 23:13:30 +0200

    toggle dark mode

--- app/src/main/java/pl/edu/agh/kis/flashcards/module/main/activity/MainActivity.kt
+++ app/src/main/java/pl/edu/agh/kis/flashcards/module/main/activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package pl.edu.agh.kis.flashcards.module.main.activity
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import pl.edu.agh.kis.flashcards.R
 
 class MainActivity : AppCompatActivity(){
@@ -16,6 +19,20 @@ class MainActivity : AppCompatActivity(){
         val inflater = menuInflater
         inflater.inflate(R.menu.example_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.toggleDarkMode -> {
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_YES ->
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    Configuration.UI_MODE_NIGHT_NO ->
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+        }
+         return true
     }
 
 }
